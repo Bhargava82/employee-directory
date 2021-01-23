69ebd54f3b300b2472d5e8c6dee92f487c05e758
Bhargava Patel <bhargavapatel82@gmail.com>
2021-01-23 16:54:22 -0500

    added hash router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import Table from "./Components/Table/Table";
 import Employee from "./Components/Employee/Employee";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
